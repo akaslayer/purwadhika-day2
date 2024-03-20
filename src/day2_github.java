6979053b6913e664337b9f0524f530d5df088505
akaslayer <hendrysurijanto123@gmail.com>
2024-03-20 15:36:11 +0700

    Bedain

--- src/day2_github.java
+++ src/day2_github.java
@@ -1,10 +1,5 @@
 public class day2_github {
     public static void main(String[] args) {
-        System.out.println("Hendry is here in github");
-        System.out.println("hahaha");
-        System.out.println("laki-laki");
-        System.out.println("ROARRRRRR");
-        System.out.println("mamamlala");
-
+        System.out.println("jaha");
     }
 }

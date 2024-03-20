a744b46c0931e04ad1520232b8b33f9159ec617d
akaslayer <hendrysurijanto123@gmail.com>
2024-03-20 16:08:14 +0700

    add 4 new change

--- src/day2_github.java
+++ src/day2_github.java
@@ -1,5 +1,8 @@
 public class day2_github {
     public static void main(String[] args) {
         System.out.println("Change");
+        System.out.println("Change2");
+        System.out.println("Change3");
+        System.out.println("Change4");
     }
 }

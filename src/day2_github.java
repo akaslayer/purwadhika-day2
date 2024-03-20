4076650ac8b46946dec8ca931b82ac9a143564d3
akaslayer <hendrysurijanto123@gmail.com>
2024-03-20 16:15:46 +0700

    newBranch2

--- src/day2_github.java
+++ src/day2_github.java
@@ -1,8 +1,5 @@
 public class day2_github {
     public static void main(String[] args) {
-        System.out.println("Change5");
-        System.out.println("Change");
-        System.out.println("Change5");
-        System.out.println("Change");
+
     }
 }

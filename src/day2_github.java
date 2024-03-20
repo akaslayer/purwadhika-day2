6a9cd6139310ec9eff21caaeafead8441aff49f2
akaslayer <hendrysurijanto123@gmail.com>
2024-03-20 16:13:15 +0700

    add new print

--- src/day2_github.java
+++ src/day2_github.java
@@ -2,6 +2,7 @@ public class day2_github {
     public static void main(String[] args) {
         System.out.println("Change5");
         System.out.println("Change");
-
+        System.out.println("Change5");
+        System.out.println("Change");
     }
 }
